ad5653796113eb1414cf0b61ea18c43a9eb0e3c3
Jakub Rzepinski <rzepa8@gmail.com>
2023-02-27 16:45:18 +0000

    fixed code to pass all test

--- index.js
+++ index.js
@@ -12,4 +12,39 @@ const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the manager name?',
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is this employee ID?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is this employee email?',
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'What is this employee office number?',
+    },
+    {
+      type: 'list',
+      message: 'What would you like to do?',
+      name: 'menu',
+      choices: ['Add an engineer', 'Add an intern', 'Finish building team'],
+    },
+    
+  ])
+  .then((data) => {
+    const manager1 = new Manager(data.id, data.name, data.email, data.officeNumber);
+    render();
+  });
 
+//render();
